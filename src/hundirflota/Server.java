12a5c22f0b1c3b6431c077515354877e866d92ce
byronnDev <mikel.e.g.2004@gmail.com>
2024-05-14 10:08:41 +0200

    Extract gameData to other file

--- src/hundirflota/Server.java
+++ src/hundirflota/Server.java
@@ -87,66 +87,6 @@ public class Server {
         }
     }
 
-    private class GameData {
-        char[][] mapaUsuario;
-        char[][] mapaOrdenador;
-        char[][] mapaOrdenadorParaUsuario;
-        int puntosUsuario;
-        int puntosOrdenador;
-        boolean juegoTerminado;
-
-        public char[][] getMapaUsuario() {
-            return mapaUsuario;
-        }
-
-        public void setMapaUsuario(char[][] mapaUsuario) {
-            this.mapaUsuario = mapaUsuario;
-        }
-
-        public char[][] getMapaOrdenador() {
-            return mapaOrdenador;
-        }
-
-        public void setMapaOrdenador(char[][] mapaOrdenador) {
-            this.mapaOrdenador = mapaOrdenador;
-        }
-
-        public char[][] getMapaOrdenadorParaUsuario() {
-            return mapaOrdenadorParaUsuario;
-        }
-
-        public void setMapaOrdenadorParaUsuario(char[][] mapaOrdenadorParaUsuario) {
-            this.mapaOrdenadorParaUsuario = mapaOrdenadorParaUsuario;
-        }
-
-        public int getPuntosUsuario() {
-            return puntosUsuario;
-        }
-
-        public void setPuntosUsuario(int puntosUsuario) {
-            this.puntosUsuario = puntosUsuario;
-        }
-
-        public int getPuntosOrdenador() {
-            return puntosOrdenador;
-        }
-
-        public void setPuntosOrdenador(int puntosOrdenador) {
-            this.puntosOrdenador = puntosOrdenador;
-        }
-
-        public boolean isJuegoTerminado() {
-            return juegoTerminado;
-        }
-
-        public void setJuegoTerminado(boolean juegoTerminado) {
-            this.juegoTerminado = juegoTerminado;
-        }
-
-        public GameData() {
-        }
-    }
-
     private void playGame() {
         GameData gameData = new GameData();
         gameData.setMapaUsuario(new char[TAMANIO][TAMANIO]);
